ffa077e49c89192c33a4b19717ccc269cb0e7267
iriquelmee <i.riquelmee@duocuc.cl>
2025-07-02 00:33:44 -0400

    funcion lambda que realiza registro de usuario en base datos mongo despues del registro cognito, solo apunta a endpoint hacia componente bff y manda data usuario

---
+++ index.mjs
@@ -0,0 +1,45 @@
+export const handler = async (event) => {
+    const data = event.request.userAttributes;
+  
+    // modelo cognito
+    const user = {
+      userID: data.sub,
+      name: data.name || '',
+      email: data.email || '',
+      nickname: data.nickname || '',
+      perfil: data['custom:perfil'] || '',
+      userTypeId: 'default',
+      userSubTypeId: 'basic',
+      status: 'active'
+    };
+  
+    console.log("Usuario completo:", JSON.stringify(user));
+  
+    // modelo bff 
+    const bffUserData = {
+      UserID: user.userID,
+      name: user.name,
+      UserTypeId: user.userTypeId,
+      UserSubTypeId: user.userSubTypeId,
+      Status: user.status
+    };
+  
+    const endpoint = 'http://certichaindata.ddns.net:8083/api/userdata';
+  
+    try {
+      const res = await fetch(endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(bffUserData)
+      });
+  
+      const responseText = await res.text();
+      console.log('Respuesta del BFF:', res.status, responseText);
+    } catch (err) {
+      console.error('Error al llamar al BFF:', err);
+    }
+  
+    return event;
+  };
